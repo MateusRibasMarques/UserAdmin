55b95234d7bfe3642e76f607af8eed3c85974cea
MATEUS_MARCAL_RIBAS_MARQUES <12200140@OMEGAB37.BARROCA>
2024-10-18 09:48:57 -0300

    login

--- Client/src/Pages/Login/Login.jsx
+++ Client/src/Pages/Login/Login.jsx
@@ -1,12 +1,32 @@
-import styles from '../Login/Login.module.css'
+import styles from '../Login/Login.module.css';
+
 function Login() {
-
-
     return (
-      <>
-     
-      </>
-    )
-  }
-  
-  export default Login
+        <div className={styles.formContainer}>
+            <form className={styles.form}>
+                <span className={styles.inputSpan}>
+                    <label htmlFor="email" className={styles.label}>Email</label>
+                    <input type="email" name="email" id="email" />
+                </span>
+                <span className={styles.inputSpan}>
+                    <label htmlFor="password" className={styles.label}>Password</label>
+                    <input type="password" name="password" id="password" />
+                </span>
+                <div className={styles.radioInputs}>
+                    <label className={styles.radio}>
+                        <input type="radio" name="radio" defaultChecked />
+                        <span className={styles.name}>Usuário</span>
+                    </label>
+                    <label className={styles.radio}>
+                        <input type="radio" name="radio" />
+                        <span className={styles.name}>Administrador</span>
+                    </label>
+                </div>
+                <input className={styles.submit} type="submit" value="Log in" />
+                
+            </form>
+        </div>
+    );
+}
+
+export default Login;
