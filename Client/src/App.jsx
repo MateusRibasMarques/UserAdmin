8091237bac24dae2cfb90ca3159bbad42b763697
MATEUS_MARCAL_RIBAS_MARQUES <12200140@OMEGAB37.BARROCA>
2024-10-18 09:38:39 -0300

    first commit

---
+++ Client/src/App.jsx
@@ -0,0 +1,15 @@
+
+import Login from './Pages/Login/Login'
+
+
+function App() {
+
+
+  return (
+    <>
+   <Login/>
+    </>
+  )
+}
+
+export default App
---
+++ Client/src/Pages/Login/Login.jsx
@@ -0,0 +1,12 @@
+import styles from '../Login/Login.module.css'
+function Login() {
+
+
+    return (
+      <>
+     
+      </>
+    )
+  }
+  
+  export default Login
